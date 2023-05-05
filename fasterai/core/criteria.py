7650c98b283c3825bd436efa8ad338fc15ef0032
nathanhubens <nathan.hubens@gmail.com>
2023-05-05 10:39:14 +0200

    Changed granularity and improved sparsifier

--- fasterai/core/criteria.py
+++ fasterai/core/criteria.py
@@ -16,43 +16,66 @@ from .granularity import *
 
 # %% ../../nbs/00b_core.criteria.ipynb 6
 class Criteria():
-    def __init__(self, f, needs_init=False, needs_update=False, output_f=None, return_init=False):
+    def __init__(self, f, reducer='mean', normalizer=None, needs_init=False, needs_update=False, output_f=None, return_init=False):
         store_attr()
         assert (needs_init and needs_update)==False, "The init values will be overwritten by the updating ones."
-        self.min_value=None
-        
-    def __call__(self, m):
+         
+    @torch.no_grad()    
+    def __call__(self, m, g):
+        try:
+            dim = Granularities.get_dim(m, g)
+        except KeyError:
+            raise NotImplementedError('Invalid granularity')
+            
         if self.needs_update and hasattr(m, '_old_weights') == False:
             m.register_buffer("_old_weights", m._init_weights.clone()) # If the previous value of weights is not known, take the initial value
-
+            
         wf = self.f(m.weight)
+        
         if self.needs_init: wi = self.f(m._init_weights)
         if self.needs_update: wi = self.f(m._old_weights)
-
+        
         if hasattr(m, '_mask') == False: m.register_buffer("_mask", torch.ones_like(wf)) # Put the mask into a buffer
+        
+        if self.output_f: scores = self.output_f(wf, wi)
+        elif self.return_init: scores = wi
+        else: scores = wf
+            
+        scores = self._rescale(scores).mul_(m._mask)
+        scores = self._reduce(scores, dim)
+        scores = self._normalize(scores)
+        return scores
+    
+    def _reduce(self, scores, dim):
+        if self.reducer == "sum":
+            return scores[None].sum(dim=dim, keepdim=True).squeeze(0)
+        elif self.reducer == "mean":
+            return scores[None].mean(dim=dim, keepdim=True).squeeze(0)
+        else: 
+            raise NotImplementedError('Invalid reducer')
+            
+    def _normalize(self, scores):
+        if self.normalizer is None: 
+            return scores
+        elif isinstance(self.normalizer, Callable):
+            return self.normalizer(scores)
+        elif self.normalizer == "sum":
+            return scores / scores.sum()
+        elif self.normalizer == "standardization":
+            return (scores - scores.min()) / (scores.max() - scores.min()+torch.finfo(torch.float32).eps)
+        elif self.normalizer == "mean":
+            return scores / scores.mean()
+        elif self.normalizer == "max":
+            return scores / scores.max()
+        elif self.normalizer == 'gaussian':
+            return (scores - scores.mean()) / (scores.std()+torch.finfo(torch.float32).eps)
+        else:
+            raise NotImplementedError('Invalid normalizer')
 
-        if self.output_f: output = self.output_f(wf, wi)
-        elif self.return_init: output = wi
-        else: output = wf
-        return output
-    
-    def granularize(self, m, scores, g): # Put the scores into granularities
-        if g in granularities[m.__class__.__name__]:
-            dim = granularities[m.__class__.__name__][g]
-            scores = scores[None].mean(dim=dim, keepdim=True).squeeze(0)
-        else: raise NameError('Invalid Granularity')
+    def _rescale(self, scores): # Rescale scores to be >0, thus avoiding not pruning previously pruned weight (with a value of 0)
+        scores += scores.min().abs() + torch.finfo(torch.float32).eps
         return scores
-    
-    def get_scores(self, m, g, rescale=False, min_value=None):  
-        if rescale: scores = self._rescale(self(m), min_value).mul_(m._mask)
-        scores = self.granularize(m, self(m), g)
-        return scores
-    
-    def _rescale(self, w, min_value): # Rescale scores to be >0, thus avoiding not pruning previously pruned weight (with a value of 0)
-        self.min_value = min_value if min_value else w.min()
-        output =  w + self.min_value.abs() + torch.finfo(torch.float32).eps
-        return output
-    
+
     def update_weights(self, m):
         if self.needs_update: 
             m._old_weights = m.weight.data.clone() # The current value becomes the old one for the next iteration
@@ -60,51 +83,51 @@ class Criteria():
 # %% ../../nbs/00b_core.criteria.ipynb 9
 random = Criteria(torch.randn_like)
 
-# %% ../../nbs/00b_core.criteria.ipynb 15
+# %% ../../nbs/00b_core.criteria.ipynb 12
 large_final = Criteria(torch.abs)
 
-# %% ../../nbs/00b_core.criteria.ipynb 18
+# %% ../../nbs/00b_core.criteria.ipynb 15
 squared_final = Criteria(torch.square)
 
-# %% ../../nbs/00b_core.criteria.ipynb 21
+# %% ../../nbs/00b_core.criteria.ipynb 18
 small_final = Criteria(compose(torch.abs, torch.neg))
 
-# %% ../../nbs/00b_core.criteria.ipynb 24
+# %% ../../nbs/00b_core.criteria.ipynb 21
 large_init = Criteria(torch.abs, needs_init=True, return_init=True)
 
-# %% ../../nbs/00b_core.criteria.ipynb 27
+# %% ../../nbs/00b_core.criteria.ipynb 24
 small_init = Criteria(compose(torch.abs, torch.neg), needs_init=True, return_init=True)
 
-# %% ../../nbs/00b_core.criteria.ipynb 30
+# %% ../../nbs/00b_core.criteria.ipynb 27
 large_init_large_final = Criteria(torch.abs, needs_init=True, output_f=torch.min)
 
-# %% ../../nbs/00b_core.criteria.ipynb 33
+# %% ../../nbs/00b_core.criteria.ipynb 30
 small_init_small_final = Criteria(torch.abs, needs_init=True, output_f=lambda x,y: torch.neg(torch.max(x,y)))
 
-# %% ../../nbs/00b_core.criteria.ipynb 36
+# %% ../../nbs/00b_core.criteria.ipynb 33
 magnitude_increase = Criteria(torch.abs, needs_init=True, output_f= torch.sub)
 
-# %% ../../nbs/00b_core.criteria.ipynb 39
+# %% ../../nbs/00b_core.criteria.ipynb 36
 movement = Criteria(noop, needs_init=True, output_f= lambda x,y: torch.abs(torch.sub(x,y)))
 
-# %% ../../nbs/00b_core.criteria.ipynb 44
+# %% ../../nbs/00b_core.criteria.ipynb 41
 updating_magnitude_increase = Criteria(torch.abs, needs_update=True, output_f= lambda x,y: torch.sub(x,y))
 
-# %% ../../nbs/00b_core.criteria.ipynb 47
+# %% ../../nbs/00b_core.criteria.ipynb 44
 updating_movement = Criteria(noop, needs_update=True, output_f= lambda x,y: torch.abs(torch.sub(x,y)))
 
-# %% ../../nbs/00b_core.criteria.ipynb 50
+# %% ../../nbs/00b_core.criteria.ipynb 47
 movmag = Criteria(noop, needs_init=True, output_f=lambda x,y: torch.abs(torch.mul(x, torch.sub(x,y))))
 
-# %% ../../nbs/00b_core.criteria.ipynb 53
+# %% ../../nbs/00b_core.criteria.ipynb 50
 updating_movmag = Criteria(noop, needs_update=True, output_f=lambda x,y: torch.abs(torch.mul(x, torch.sub(x,y))))
 
-# %% ../../nbs/00b_core.criteria.ipynb 55
+# %% ../../nbs/00b_core.criteria.ipynb 52
 criterias = ('random', 'large_final', 'small_final', 'squared_final', 'small_init', 'small_final', 'large_init_large_final', 'small_init_small_final', 'magnitude_increase', 'movement', 'updating_magnitude_increase', 'updating_movement', 'updating_movmag')
 def available_criterias():
     print(criterias)
 
-# %% ../../nbs/00b_core.criteria.ipynb 76
+# %% ../../nbs/00b_core.criteria.ipynb 73
 def grad_crit(m, g):
     if g in granularities[m.__class__.__name__]: 
         dim = granularities[m.__class__.__name__][g]
--- fasterai/sparse/sparsifier.py
+++ fasterai/sparse/sparsifier.py
@@ -18,15 +18,18 @@ class Sparsifier():
     def __init__(self, model, granularity, context, criteria, layer_type=nn.Conv2d):
         store_attr()
         self._save_weights() # Save the original weights
+        self._reset_threshold()
 
     def prune_layer(self, m, sparsity, round_to=None):
-        scores = self.criteria(m)
-        setattr(m, '_mask', self._compute_mask(m, scores, sparsity, round_to))
+        scores    = self._compute_scores(m, sparsity)
+        threshold = self._compute_threshold(scores, sparsity, round_to)
+        mask      = self._compute_mask(scores, threshold)
+        setattr(m, '_mask', mask)
         self._apply(m)
         self.criteria.update_weights(m)
 
     def prune_model(self, sparsity, round_to=None):
-        self.threshold=None
+        self._reset_threshold()
         sparsity_list = listify(sparsity)
         if len(sparsity_list)>1: assert self.context=='local', f"A list of sparsities cannot be passed using: {self.context}"
         sparsities = cycle(sparsity_list) if len(sparsity_list)==1 else iter(sparsity_list)
@@ -39,7 +42,7 @@ class Sparsifier():
                 
     def prune_batchnorm(self, m, bn):
         mask = getattr(m, "_mask", None)
-        if self.granularity == 'filter' and mask:
+        if self.granularity == 'filter' and true(mask):
             bn.weight.data.mul_(mask.squeeze())
             bn.bias.data.mul_(mask.squeeze())
             
@@ -87,30 +90,34 @@ class Sparsifier():
                 if hasattr(m, '_mask'): del m._buffers["_mask"]
                 if hasattr(m, '_init_weights'): del m._buffers["_init_weights"]
                 if hasattr(m, '_init_biases'): del m._buffers["_init_biases"]
-    
-    def _compute_threshold(self, m, scores, sparsity):
-        if self.context == 'global':
-            if self.threshold is None: 
-                global_criteria = torch.cat([self.criteria(m).view(-1) for m in self.model.modules() if isinstance(m, self.layer_type)]) # Get all scores
-                global_scores = torch.cat([self.criteria.get_scores(m, self.granularity, True, global_criteria.min()).view(-1) for m in self.model.modules() if isinstance(m, self.layer_type)])
-                self.threshold = torch.quantile(global_scores, sparsity/100) # Compute the threshold globally (only once per model pruning)
-            scores = self.criteria.get_scores(m, self.granularity, True, self.criteria.min_value) # min_value is computed only once per prune_model
-            return self.threshold, scores
-        elif self.context == 'local':
-            scores = self.criteria.get_scores(m, self.granularity)
-            return torch.quantile(scores.view(-1), sparsity/100), scores
-        else: raise NameError('Invalid Context')
-
+                    
+    def _reset_threshold(self):
+        self.threshold=None
+            
     def _rounded_sparsity(self, n_to_prune, round_to):
         return max(round_to*torch.ceil(n_to_prune/round_to), round_to)
     
-    def _compute_mask(self, m, scores, sparsity, round_to):
-        self.threshold, scores = self._compute_threshold(m, scores, sparsity)
+    def _compute_scores(self, m, sparsity):
+        if self.context == 'global':
+            if self.threshold == None: 
+                global_scores  = torch.cat([self.criteria(m, self.granularity).view(-1) for m in self.model.modules() if isinstance(m, self.layer_type)])
+                self.threshold = torch.quantile(global_scores.view(-1), sparsity/100)
+            local_scores = self.criteria(m, self.granularity)
+        elif self.context == 'local': 
+            local_scores = self.criteria(m, self.granularity)
+            self.threshold = torch.quantile(local_scores.view(-1), sparsity/100)
+        else: raise NameError('Invalid Context')
+        return local_scores
+                
+    def _compute_threshold(self, scores, sparsity, round_to):
         if round_to:
             n_to_keep = sum(scores.ge(self.threshold)).squeeze()
             self.threshold = torch.topk(scores.squeeze(), int(self._rounded_sparsity(n_to_keep, round_to)))[0].min()
-        if self.threshold > scores.max(): self.threshold = scores.max() # Make sure we don't remove every weight of a given layer
-        return scores.ge(self.threshold).to(dtype=scores.dtype)
+        return self.threshold
+    
+    def _compute_mask(self, scores, threshold):
+        if threshold > scores.max(): threshold = scores.max() # Make sure we don't remove every weight of a given layer
+        return scores.ge(threshold).to(dtype=scores.dtype)
     
     def print_sparsity(self):
         for k,m in enumerate(self.model.modules()):
